896e81fe31c647faa38a97427cd572b9e9226a36
Anna Concepcion <anna.concepcion@gmail.com>
2016-05-23 14:55:33 -0400

    Adds more posting styles and additional comments field

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20160519044220) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "postings", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "pet_type"
+    t.string   "street_address"
+    t.string   "city"
+    t.string   "state"
+    t.integer  "zipcode"
+    t.string   "start_date"
+    t.string   "end_date"
+    t.integer  "is_stay_post",   default: 0
+    t.string   "comments"
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
+  end
 
 end
